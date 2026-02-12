d276c16f8f1e3e55e103c451b73d2ff6ce3d1839
Yared Mulaw <yaredplaysrb@gmail.com>
2026-02-12 09:03:33 -0500

    Made the canvas

---
+++ projects/portrait/code.js
@@ -0,0 +1,6 @@
+const canvas = document.getElementById("portrait");
+const ctx = canvas.getContext("2d");
+ctx.fillStyle="lightBlue";
+ctx.fillRect(0, 0, 800, 800);
+ctx.fillStyle="lightBlue";
+ctx.beginPath();
