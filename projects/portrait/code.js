71b2a982dc3f7f8f50062cd8ae36a7993d9ce8f1
Yared Mulaw <yaredplaysrb@gmail.com>
2026-02-16 18:47:56 -0500

    Base face made

--- projects/portrait/code.js
+++ projects/portrait/code.js
@@ -3,4 +3,15 @@ const ctx = canvas.getContext("2d");
 ctx.fillStyle="lightBlue";
 ctx.fillRect(0, 0, 800, 800);
 ctx.fillStyle="lightBlue";
-ctx.beginPath();
+ctx.beginPath(); // Face
+ctx.arc(400, 400, 200, 0, Math.PI * 2, true);
+ctx.stroke();
+ctx.beginPath(); // Mouth
+ctx.arc(405, 405, 125, 0, Math.PI, false);
+ctx.stroke();
+ctx.beginPath(); // Left eye
+ctx.arc(350, 375, 20, 0, Math.PI * 2, true);
+ctx.stroke();
+ctx.beginPath(); // Right eye
+ctx.arc(450, 375, 20, 0, Math.PI * 2, true);
+ctx.stroke();
